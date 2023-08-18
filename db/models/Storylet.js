70df3073256a522c38c807ba490c07b09a587d34
Nicholas Gregory <nicholas.babcock092@gmail.com>
2023-08-18 10:40:07 -0700

    link method of Storylet class

--- db/models/Storylet.js
+++ db/models/Storylet.js
@@ -3,8 +3,21 @@ const sequelize = require('../connection');
 
 const NextStorylet = require('./NextStorylet');
 const Affect = require('./Affect');
+const AffectToAdvance = require('./AffectToAdvance');
 
 class Storylet extends Model {
+
+    async link(storylet, affects) {
+        const link = await NextStorylet.create({
+            next: storylet.id,
+            current: this.id
+        });
+        
+        await AffectToAdvance.bulkCreate(affects.map(affect => ({
+            next: storylet.id,
+            affectId: affect.id
+        })));
+    }
     
     async affectsAndNexts() {
         const affectsAndLinks = await NextStorylet.findAll({
